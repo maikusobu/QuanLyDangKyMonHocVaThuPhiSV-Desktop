53474f96680edacf6506a78454c7f7ef84ca2086
Thy Tran <22521461@gm.uit.edu.vn>
2024-05-08 12:31:24 +0700

    feat: sidebar items

--- src/renderer/views/finance-department/FinanceDepartment.vue
+++ src/renderer/views/finance-department/FinanceDepartment.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="flex">
-    <SideBar title="phòng kế hoạch tài chính" />
+    <SideBar
+      title="phòng kế hoạch tài chính"
+      :routes="[
+        { name: 'bảng điều khiển', path: '/finance-department' },
+        { name: 'đăng ký học phần', path: '/finance-department/registration' },
+        { name: 'biên lai học phí', path: '/finance-department/history' },
+        { name: 'báo cáo học phí', path: '/finance-department/report' },
+      ]"
+    />
     <div class="flex flex-col grow">
       <Topbar />
       <RouterView />
